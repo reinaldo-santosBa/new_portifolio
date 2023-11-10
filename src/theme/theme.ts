d1beef44dcbccad1b38f5c731c14dfc9f3a75d31
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-11-10 11:20:06 -0300

    feat: add dark theme in theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,17 @@
+export const darkTheme = {
+	button : {
+		primary: '#27AE60',
+		outline: '#fafafa'
+	},
+	background: {
+		backgroundPrimary: '#171717',
+		backgroundSecondary: '#212121', 
+	},
+	fonts:{
+		fontPrimary: '#FFFFFF',
+		fontSecondary: '#4f4f4f'
+	},
+	input:{
+		inputBorder: '#4f4f4f' 
+	}
+};
