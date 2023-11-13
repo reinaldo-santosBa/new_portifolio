506922f74205c0b4905efab5b3e20111154c8b12
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-11-13 10:03:24 -0300

    feat: add components banner and about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './global/style';
 import { darkTheme, lightTheme } from './theme/theme';
 import { Menu } from './components/menu/menu';
 import { AboutSection } from './components/about/about';
+import { BannerSecion } from './components/banner/banner';
 const App: React.FC = () => {
-	const [theme, setTheme] = React.useState('light');
-
+	const [theme, setTheme] = React.useState('');
+	useEffect(()=>{
+		const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		if (prefersDarkMode) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		} 
+	},[]);
 	const toggleTheme = () => {
 		setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
 	};
@@ -14,6 +22,7 @@ const App: React.FC = () => {
 		<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
 			<GlobalStyles />
 			<Menu mode={theme} toggleTheme={toggleTheme}/>
+			<BannerSecion/>
 			<AboutSection/>
 		</ThemeProvider>
 	);
