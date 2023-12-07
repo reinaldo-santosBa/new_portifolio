2a8c912dca282dddcf8464ac16dacfbde7266ba0
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-07 17:12:42 -0300

    feat: add for a new section

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,30 @@ import { Menu } from './components/menu/menu';
 import { AboutSection } from './components/about/about';
 import { BannerSecion } from './components/banner/banner';
 import { ServiceSection } from './components/services/services';
+import { SkillsSections } from './components/skills/skills';
+import { ContactSection } from './components/contact/contact';
 const App: React.FC = () => {
 	const [theme, setTheme] = React.useState('');
-	useEffect(()=>{
+	useEffect(() => {
 		const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 		if (prefersDarkMode) {
 			setTheme('dark');
 		} else {
 			setTheme('light');
-		} 
-	},[]);
+		}
+	}, []);
 	const toggleTheme = () => {
 		setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
 	};
 	return (
 		<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
 			<GlobalStyles />
-			<Menu mode={theme} toggleTheme={toggleTheme}/>
-			<BannerSecion/>
-			<AboutSection/>
-			<ServiceSection/>
+			<Menu mode={theme} toggleTheme={toggleTheme} />
+			<BannerSecion />
+			<AboutSection />
+			<ServiceSection />
+			<SkillsSections />
+			<ContactSection />
 		</ThemeProvider>
 	);
 };
